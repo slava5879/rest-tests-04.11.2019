9d715395ee88690f21c1b0959c77843c6c2a367a
viacheslav <lan1457@ukr.net>
2019-11-26 12:12:53 +0100

    lesson 7. refactor. added new methods in PetEndPoint class

--- src/test/java/CreatePetTest.java
+++ src/test/java/CreatePetTest.java
@@ -1,78 +1,74 @@
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
-import io.restassured.specification.RequestSpecification;
-import org.junit.FixMethodOrder;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runners.MethodSorters;
 
-import static io.restassured.RestAssured.post;
 import static org.hamcrest.Matchers.anyOf;
 import static org.hamcrest.core.Is.is;
 
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class CreatePetTest {
-    static long petId;
+    private PetEndpoint petEndPoint = new PetEndpoint();
+    private static long petId;
 
-    public RequestSpecification given() {
-        return RestAssured
-                .given()
-                .baseUri("https://petstore.swagger.io/v2")
-                .log().all()
-                .contentType(ContentType.JSON);
+    private String body = "{\n" +
+            "  \"id\": 0,\n" +
+            "  \"category\": {\n" +
+            "    \"id\": 0,\n" +
+            "    \"name\": \"string\"\n" +
+            "  },\n" +
+            "  \"name\": \"cat\",\n" +
+            "  \"photoUrls\": [\n" +
+            "    \"string\"\n" +
+            "  ],\n" +
+            "  \"tags\": [\n" +
+            "    {\n" +
+            "      \"id\": 0,\n" +
+            "      \"name\": \"string\"\n" +
+            "    }\n" +
+            "  ],\n" +
+            "  \"status\": \"available\"\n" +
+            "}";
+
+    @Before
+    public void beforeMethod() {
+
+        ValidatableResponse response = petEndPoint.createPet(body)
+                .statusCode(anyOf(is(200), is(201)))
+                .body("category.name", is("string"))
+                //.log().all()
+        ;
+
+        petId = response.extract().path("id");
+        System.out.println(petId);
     }
 
     @Test
-    public void test1CreatePet(){
+    public void CreatePetTest(){
 
-        String body = "{\n" +
-                "  \"id\": 0,\n" +
-                "  \"category\": {\n" +
-                "    \"id\": 0,\n" +
-                "    \"name\": \"string\"\n" +
-                "  },\n" +
-                "  \"name\": \"cat\",\n" +
-                "  \"photoUrls\": [\n" +
-                "    \"string\"\n" +
-                "  ],\n" +
-                "  \"tags\": [\n" +
-                "    {\n" +
-                "      \"id\": 0,\n" +
-                "      \"name\": \"string\"\n" +
-                "    }\n" +
-                "  ],\n" +
-                "  \"status\": \"available\"\n" +
-                "}";
-
-        ValidatableResponse response = given()
-                .body(body)
-                .post(PetEndpoint.CREATE_PET)
-                .then()
+        petEndPoint
+                .createPet(body)
                 .statusCode(anyOf(is(200), is(201)))
-                .body("category.name", is("string"))
-                .log().all();
-
-        petId = response.extract().path("id");
+                .body("category.name", is("string"));
     }
 
     @Test ()
-    public void test2GetPetById() {
+    public void GetPetById() {
 
-        given()
-                .get(PetEndpoint.GET_PET, petId)
-                .then()
+        petEndPoint
+                .getPet(petId)
                 .statusCode(is(200))
-                .body("name", is ("cat"))
-                .log().all();
+                .body("name", is ("cat"));
     }
 
     @Test
-    public void test3DeletePetById () {
+    public void DeletePetById () {
 
-        given()
-                .delete(PetEndpoint.DELETE_PET, petId)
-                .then()
-                .statusCode(200)
-                .log().all();
+        petEndPoint
+                .deletePet(petId)
+                .statusCode(200);
+
+        petEndPoint
+                .getPet(petId)
+                .statusCode(is(404))
+                .body("message", is ("Pet not found"));
     }
 }
